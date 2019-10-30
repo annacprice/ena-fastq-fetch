043966dbb677bc7e2526b378f4e82c1bc428f812
Anna Price <36504362+annacprice@users.noreply.github.com>
2019-10-30 11:46:29 +0000

    updated api selection

--- xmlparser.py
+++ xmlparser.py
@@ -11,19 +11,19 @@ def getXML(search, dataType, **kwargs):
     # download an xml file for the specified search terms	
 
     # build the url for the query and download the xml file
-    if not any(charac.isdigit() for charac in search):
-        if dataType == "read_run":
-            dataType = "sra-run"
-        
-        build_url = {"domain": dataType,
-                    "query": search
-                    }
-        response = requests.get("https://www.ebi.ac.uk/ena/browser/api/xml/textsearch", params=build_url)
-    else:
+    if all(charac.isdigit() for charac in search):
         build_url = {"accession": search,
                     "result": dataType
                     }
         response = requests.get("https://www.ebi.ac.uk/ena/browser/api/xml/links/taxon", params=build_url)
+    else:
+        if dataType == "read_run":
+            dataType = "sra-run"
+
+        build_url = {"domain": dataType,
+                    "query": search
+                    }
+        response = requests.get("https://www.ebi.ac.uk/ena/browser/api/xml/textsearch", params=build_url)
 
     # write to file
     with open('ena.xml', 'wb') as outfile:
